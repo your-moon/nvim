3db28d4e4bd256b46551ac845a7de0a520cb1bf7
muunuu <muunuu960@gmail.com>
2024-06-24 10:25:01 +0800

    nvim tree

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -2,9 +2,8 @@ local map = vim.keymap.set
 
 map("n", "K", "<cmd> Lspsaga hover_doc <cr>", { desc = "Hover Doc" })
 map("n", "gw", "<cmd> Lspsaga peek_definition <cr>", { desc = "Peek definition" })
-map("n", "gf", "<cmd> Lspsaga finder <cr>", { desc = "Find definition" })
+map("n", "gr", "<cmd> Lspsaga finder <cr>", { desc = "Find definition" })
 map("n", "gd", "<cmd> Lspsaga goto_definition <cr>", { desc = "Goto definition" })
-map("n", "<leader>ra", "<cmd> Lspsaga rename <cr>", { desc = "LSP Rename" })
 map("n", "]d", "<cmd> Lspsaga diagnostic_jump_next <cr>", { desc = "LSP jump next" })
 map("n", "[d", "<cmd> Lspsaga diagnostic_jump_prev <cr>", { desc = "LSP jump prev" })
 map("n", "<leader>ca", "<cmd> Lspsaga code_action <cr>", { desc = "LSP code action" })
@@ -20,3 +19,7 @@ map("n", "<leader>h", "<cmd> hsplit<cr>", { desc = "hsplit" })
 -- Trouble
 map("n", "<leader>xw", "<cmd> Trouble diagnostics toggle <cr>", { desc = "Trouble Diagnostics Toggle" })
 map("n", "<leader>xq", "<cmd> Trouble qflist toggle <cr>", { desc = "Trouble Quickfix Toggle" })
+
+-- Nvim tree
+map("n", "<leader>e", "<cmd> NvimTreeToggle <cr>", { desc = "NvimTree Toggle" })
+map("n", "<leader>r", "<cmd> NvimTreeFindFileToggle <cr>", { desc = "NvimTree Find File" })
