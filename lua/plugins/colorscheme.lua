8d910157696ccae74ea5e8540e86dc7892e664cb
muunuu <muunuu960@gmail.com>
2024-10-03 13:53:48 +0800

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,54 @@
 return {
+  {
+    "RRethy/base16-nvim",
+  },
+  {
+    "vague2k/vague.nvim",
+    config = function()
+      require("vague").setup {
+        -- optional configuration here
+        transparent = true,
+        style = {
+          -- "none" is the same thing as default. But "italic" and "bold" are also valid options
+          boolean = "none",
+          number = "none",
+          float = "none",
+          error = "none",
+          comments = "none",
+          conditionals = "none",
+          functions = "none",
+          headings = "bold",
+          operators = "none",
+          strings = "none",
+          variables = "none",
+
+          -- keywords
+          keywords = "none",
+          keyword_return = "none",
+          keywords_loop = "none",
+          keywords_label = "none",
+          keywords_exception = "none",
+
+          -- builtin
+          builtin_constants = "none",
+          builtin_functions = "none",
+          builtin_types = "none",
+          builtin_variables = "none",
+        },
+        colors = {
+          func = "#bc96b0",
+          keyword = "#787bab",
+          -- string = "#d4bd98",
+          string = "#8a739a",
+          -- string = "#f2e6ff",
+          -- number = "#f2e6ff",
+          -- string = "#d8d5b1",
+          number = "#8f729e",
+          -- type = "#dcaed7",
+        },
+      }
+    end,
+  },
   { "blazkowolf/gruber-darker.nvim" },
   {
     "folke/tokyonight.nvim",
@@ -7,10 +57,11 @@ return {
     config = function()
       -- vim.cmd [[colorscheme tokyonight-night]]
       -- vim.cmd [[colorscheme catppuccin-mocha]]
-      vim.cmd [[colorscheme rose-pine]]
+      -- vim.cmd [[colorscheme rose-pine]]
       -- vim.cmd [[colorscheme gruvbox]]
       -- vim.cmd [[colorscheme kanagawa-dragon]]
       -- vim.cmd [[colorscheme gruber-darker]]
+      vim.cmd [[colorscheme vague]]
     end,
   },
   {
